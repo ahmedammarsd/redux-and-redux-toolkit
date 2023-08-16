9b5603f3803edb1bcd7f56c4c18b28e8a6c12caf
ahmedammarsd <ahmed091999287@gmail.com>
2023-08-16 18:37:51 +0200

    finish course redux toolkit

--- redux-toolkit/react-rtk-demo/src/feature/user/UserList.jsx
+++ redux-toolkit/react-rtk-demo/src/feature/user/UserList.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSelector , useDispatch } from 'react-redux';
+import { fetchUsers } from './userSlice';
 
 const UserList = () => {
+  const data = useSelector( (state) => state.user);
+  const dispatch = useDispatch();
+  useEffect( () => {
+    dispatch(fetchUsers())
+  },[])
   return (
     <div>
         <h2>List Of User</h2>
+        {
+          data.loading && <div>LOADING...</div>
+        }
+        {
+          !data.loading && data.error 
+          ?
+          <div>
+            Error: {data.error}
+          </div>
+          :
+          null
+        }
+        {
+          !data.loading && data.data.length 
+          ?
+          <ul>
+            {
+              data.data.map( (user) => (
+                <li key={user.id}>{user.name}</li>
+              ))
+            }
+          </ul>
+          : 
+          null
+        }
     </div>
   )
 }
--- redux-toolkit/react-rtk-demo/src/feature/user/userSlice.js
+++ redux-toolkit/react-rtk-demo/src/feature/user/userSlice.js
@@ -10,22 +10,20 @@ const initialState = {
 // Generated pending , fukfikked , rejected action types
 export const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
     return axios.get("https://jsonplaceholder.typicode.com/users")
-    .then((res) => {
-        res.data.map( (user) => user.id);
-    })
+    .then((res) => res.data)
 })
 const userSlice = createSlice({
     name: "user",
     initialState,
     extraReducers:(builder) => {
         builder.addCase(fetchUsers.pending , (state) => {
-            state.loading = true;
-        }),
-        builder.addCase(fetchUsers.fulfilled , (state , action) => {
+            state.loading = true
+        } );
+        builder.addCase(fetchUsers.fulfilled ,(state , action) => {
             state.loading = false;
             state.data = action.payload;
             state.error = "";
-        }),
+        });
         builder.addCase(fetchUsers.rejected , (state , action) => {
             state.loading = false;
             state.data = [];
